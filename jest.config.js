f9a23bc7946db64118b935f52dd190c5e20022f2
Jon Saunders <jon@jsau.co.uk>
2019-12-29 20:23:23 +0000

    Update coverage collection

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   bail: 1,
   collectCoverage: true,
+  collectCoverageFrom: [
+    './src/**/*.js',
+    '!./src/typedef/**',
+  ],
   errorOnDeprecated: true,
   notify: true,
   verbose: true,
