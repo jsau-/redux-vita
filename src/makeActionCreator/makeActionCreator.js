e4dd292fadc663f472084e34284b9c465df0eb34
Jon Saunders <jon@jsau.co.uk>
2020-01-04 21:07:48 +0000

    Validate makeActioCreator params on call, not as part of created function

--- src/makeActionCreator/makeActionCreator.js
+++ src/makeActionCreator/makeActionCreator.js
@@ -11,28 +11,35 @@ import { KEY_TYPE } from './constants';
  * @throws {Error} If action type is empty, or additional generated properties
  * were not a plain object.
  */
-export default (strType, ufuncPropertyCreator) => (...varargsPropertyCreator) => {
+export default (strType, ufuncPropertyCreator) => {
+  /**
+   * NB: Param validation has been separated from the returned function body
+   * since we should only have to do it _once_ rather than on each function
+   * call.
+   */
   if (isEmpty(strType)) {
     throw new Error('Type must not be empty.');
   }
 
-  const objAdditionalProperties = isNil(ufuncPropertyCreator) ?
-    {} :
-    ufuncPropertyCreator(...varargsPropertyCreator);
+  return (...varargsPropertyCreator) => {
+    const objAdditionalProperties = isNil(ufuncPropertyCreator) ?
+      {} :
+      ufuncPropertyCreator(...varargsPropertyCreator);
 
-  /*
-   * Sanity-check our property creator function actually generated a plain
-   * object (otherwise we'll run into issues w/ Redux later...)
-   */
-  if (!isPlainObject(objAdditionalProperties)) {
-    throw new Error(
-      `Property creator function generated an invalid type. Expected plain \
+    /**
+     * Sanity-check our property creator function actually generated a plain
+     * object (otherwise we'll run into issues w/ Redux later...)
+     */
+    if (!isPlainObject(objAdditionalProperties)) {
+      throw new Error(
+        `Property creator function generated an invalid type. Expected plain \
 object, got '${typeof objAdditionalProperties}'`,
-    );
-  }
+      );
+    }
 
-  return {
-    ...objAdditionalProperties,
-    [KEY_TYPE]: strType,
+    return {
+      ...objAdditionalProperties,
+      [KEY_TYPE]: strType,
+    };
   };
 };
