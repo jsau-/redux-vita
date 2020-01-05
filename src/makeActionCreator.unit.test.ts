c6439f3871cbf06e9f45a77642e59bf9db895e9d
Jon Saunders <jon@jsau.co.uk>
2020-01-05 21:27:42 +0000

    Fix makeActionCreator unit tests

--- src/makeActionCreator.unit.test.ts
+++ src/makeActionCreator.unit.test.ts
@@ -2,40 +2,39 @@ import { makeActionCreator } from './makeActionCreator';
 
 describe('makeActionCreator', () => {
   it('Should throw if type is empty', () => {
-    expect(() => makeActionCreator()).toThrow('Type must not be empty.');
+    expect(() => makeActionCreator('', () => ({}))).toThrow('Type must not be empty.');
   });
 
   it('Should create an action with type', () => {
-    const strType = 'type';
-    const actionCreator = makeActionCreator(strType);
-
-    expect(actionCreator()).toEqual({ type: strType });
+    const type = 'type';
+    const actionCreator = makeActionCreator(type, () => ({}));
+    expect(actionCreator()).toEqual({ type: type });
   });
 
   it('Should invoke the additional property creator function if provided', () => {
-    const strType = 'type';
+    const type = 'type';
 
     const mockFuncProperty = jest.fn((paramOne, paramTwo) => ({ paramOne, paramTwo }));
 
-    const actionCreator = makeActionCreator(strType, mockFuncProperty);
+    const actionCreator = makeActionCreator(type, mockFuncProperty);
 
-    const mixedActionCreatorParamOne = 1;
-    const mixedActionCreatorParamTwo = 2;
-    const objCreatedAction = actionCreator(mixedActionCreatorParamOne, mixedActionCreatorParamTwo);
+    const paramOne = 1;
+    const paramTwo = 2;
+    const createdAction = actionCreator(paramOne, paramTwo);
 
-    expect(objCreatedAction).toEqual({ type: strType, paramOne: 1, paramTwo: 2 });
+    expect(createdAction).toEqual({ type: type, paramOne: 1, paramTwo: 2 });
 
     expect(mockFuncProperty).toHaveBeenCalledTimes(1);
-    expect(mockFuncProperty.mock.calls[0][0]).toBe(mixedActionCreatorParamOne);
-    expect(mockFuncProperty.mock.calls[0][1]).toBe(mixedActionCreatorParamTwo);
+    expect(mockFuncProperty.mock.calls[0][0]).toBe(paramOne);
+    expect(mockFuncProperty.mock.calls[0][1]).toBe(paramTwo);
   });
 
   it('Should throw on generating additional properties if not a plain object', () => {
-    const strType = 'type';
+    const type = 'type';
 
-    const mockFuncProperty = jest.fn(() => 'invalid_type');
+    const mockFuncProperty = jest.fn(() => ({ invalid_type: 'invalid_type' }));
 
-    const actionCreator = makeActionCreator(strType, mockFuncProperty);
+    const actionCreator = makeActionCreator(type, mockFuncProperty);
 
     expect(() => actionCreator())
       .toThrow('Property creator function generated an invalid type. Expected plain object, got \'string\'');
